14bbb7ff9dff1777f8ac5967c2cbe8b0540d460b
vivigm2006 <vivigilsonm@gmail.com>
2026-03-28 11:34:27 -0400

    Tercer adelanto del Dashboard

--- app2.py
+++ app2.py
@@ -7,6 +7,7 @@ import plotly.express as px
 #titulo de la pestaña del navegador
 st.set_page_config(page_title="Dashboard del Análisis de Divergencia Cultural en Videojuegos", layout="wide")
 
+#estetica del trabajo, para que parezcan cuadrados con sombra y bordes redondeados
 st.markdown("""
     <style>
     .stApp { background-color: #f8f9fa; }
@@ -77,3 +78,64 @@ with col2:
 #para que el usuario controle la cantidad que quiere ver de la tabla
 with col3:
     top_n = st.number_input("Ver Top N juegos:", 5, 50, 10)
+
+#organizacion de los tabs y sus nombres
+tab1, tab2, tab3, tab4, tab5 = st.tabs([
+    "📈 Volúmenes de Venta", 
+    "🧪 Comparativa Regional", 
+    "🚀 Factor Hardware", 
+    "📂 Evidencia Detallada",
+    "🔍 Casos Atípicos"
+])
+
+#tab1, con respecto al primer objetivo de la investigacion
+with tab1:
+    st.header("1. Cuantificación de Preferencias por Género")
+#se divide en 2 columnas para aprovechar el espacio
+    col_bar, col_pie = st.columns(2)
+
+#un grafico de barras, eje horzontal generos y vertical las regiones, un colo distinto segun cada variable
+    with col_bar:
+        fig1 = px.bar(df_filtrado, x="Genre", y=reg_tech, 
+                     title=f"Ventas por Género en {region_sel}",
+                     color="Genre", template="plotly_white")
+        st.plotly_chart(fig1, use_container_width=True)
+
+#un grafico circular para ver el peso de cada género
+    with col_pie:
+        fig_pie = px.pie(df_filtrado, values=reg_tech, names='Genre',
+                         title=f"Distribución porcentual en {region_sel}",
+                         hole=0.4)
+        st.plotly_chart(fig_pie, use_container_width=True)
+
+#tab2, con respecto al segundo objetivo
+with tab2:
+    st.header("2. Similitud entre Mercados Globales")
+    
+#grafico de caja que muestra la mediana (la línea del medio) y que tan dispersos están los datos
+    st.subheader(f"Dispersión y Mediana de Ventas en {region_sel}")
+    fig_box = px.box(df_filtrado, x="Genre", y=reg_tech, color="Genre",
+                    title=f"Distribución de éxito por título en {region_sel}",
+                    points="outliers", template="plotly_white")
+    st.plotly_chart(fig_box, use_container_width=True)
+    st.divider()
+
+#titulo del segundo grafico en este mismo tab
+    st.subheader("Matriz de Identidad de Consumo")
+    
+#cambiamos el nombre tecnico por nombres bonitos
+    df_corr = df_filtrado[["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales"]].rename(columns={
+        "NA_Sales": "Norteamérica",
+        "EU_Sales": "Europa",
+        "JP_Sales": "Japón",
+        "Other_Sales": "Otros"
+    })
+#calculo de la correlación (un número entre 0 y 1)
+    corr_matrix = df_corr.corr()
+
+#dibujo del mapa de colores 
+    fig_corr = px.imshow(corr_matrix, 
+                         text_auto=True, 
+                         color_continuous_scale='RdYlGn',
+                         title="Correlación entre Mercados (Similitud de gustos)")
+    st.plotly_chart(fig_corr, use_container_width=True)
