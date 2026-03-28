0f9b5156f3d40b04f45c56bcfaddce5756c9f243
vivigm2006 <vivigilsonm@gmail.com>
2026-03-27 20:12:51 -0400

    Primer Adelanto del Dashboard

--- app.py
+++ app.py
@@ -1,80 +1,47 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-df = pd.read_csv("12. Videogame Sales.csv")
-print(df)
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-#revisar data inicial
-df.head(5)
+#titulo de la pestaña del navegador
+st.set_page_config(page_title="Dashboard del Análisis de Divergencia Cultural en Videojuegos", layout="wide")
 
-#revision de los datos
-print("tipos de datos", df.dtypes)
-print()
-print("dimension de la data", df.shape)
+#carga de los datos
+@st.cache_data
+def load_data():
+    df = pd.read_csv("12. Videogame_Sales_Limpio.csv")
+    df = df.drop(columns=["Unnamed: 0"], errors="ignore")
+    return df
 
-#eliminar columnas que no son pertinentes en nuestra investigacion
-df.drop(columns=["Year", "Publisher", "Rank"])
+df_base = load_data()
 
-#identificar los datos malucos
-for columns in df.columns:
-    print(df[columns].value_counts())
-    print()
-    print(df[columns].isna().sum())
+#sección que dinamiza la particpacion del usurio desde su entrada
+st.sidebar.header("🕹️ Variables de Investigación")
 
-#verificar la cantidad de datos faltantes (nulos) por cada columna
-df_null = df.isnull().sum()
+#para que al usuario pueda comparar los generos y los primeros 5 como predeterminados
+generos = st.sidebar.multiselect(
+    "Selecciona los géneros:",
+    options=df_base["Genre"].unique(),
+    default=df_base["Genre"].unique()[:5]
+)
 
-#mostrar el resultado por variable
-print("Datos faltantes por columna:")
-print(df_null)
+#para que el usuario elige un nombre de la region que quiere ver
+region_sel = st.sidebar.radio(
+    "Región de análisis:",
+    options=["Norteamérica", "Europa", "Japón", "Otros", "Ventas Globales"]
+)
 
-#calcular el total de datos faltantes en todo el dataframe
-total_null = df_null.sum()
-print(f"\nTotal de datos faltantes en el dataset: {total_null}")
+dict_regiones = {
+    "Norteamérica": "NA_Sales", "Europa": "EU_Sales", 
+    "Japón": "JP_Sales", "Otros": "Other_Sales", "Ventas Globales": "Global_Sales"
+}
+reg_tech = dict_regiones[region_sel]
 
-#definimos los valores dañinos
-basura = ["NA", "ERROR", "UNKNOW"]
+#un filtrado dinámico donde se crea un nuevo dataframe
+df_filtrado = df_base[df_base["Genre"].isin(generos)]
 
-#limpieza de ventas por las regiones
-regiones = ["JP_Sales", "NA_Sales", "EU_Sales", "Other_Sales", "Global_Sales"]
-
-#convertir a número, calcular mediana y rellenar los valores nulos con sus medianas
-for reg in regiones:
-    df[reg] = pd.to_numeric(df[reg], errors="coerce")
-    mediana = df[reg].median()
-    df[reg] = df[reg].fillna(mediana)
-
-print("Limpieza completada para análisis regional")
-print(df[regiones].describe().round(2))
-
-#quitar espacios vacios 
-df["Genre"] = df["Genre"].str.strip()
-
-#limpiar texto variable de texto (Genre)
-df = df[~df["Genre"].isin(["NA", "ERROR", "UNKNOW"]) & df["Genre"].notna()]
-
-#aseguramos la misma escritura en los datos
-df["Genre"] = df["Genre"].str.capitalize()
-#verificacion
-print(df["Genre"].unique())
-
-#quitar espacios vacios y aseguramos la misma escritura
-df["Platform"] = df["Platform"].str.strip()
-
-#limpiar texto variable de texto (Platform)
-df = df[~df["Platform"].isin(["NA", "ERROR", "UNKNOW"]) & df["Platform"].notna()]
-
-#aseguramos la misma escritura en los datos
-df["Platform"] = df["Platform"].str.capitalize()
-#verificacion
-print(df["Platform"].unique())
-
-#conteo de filas están totalmente duplicadas
-print("Total de filas duplicadas:", df.duplicated().sum())
-
-#ver las filas que están repetidas
-df_duplicados = df[df.duplicated(keep=False)]
-print(df_duplicados.head())
-
-df_duplicados.to_csv("12. Videogame Sales_limpio.csv")
+#titulo y subtitulo en la pagina
+st.title("📊 Análisis de Divergencia Cultural en Videojuegos")
+st.markdown("### Investigación sobre la validez de la universalidad en el mercado global")
+st.divider()
